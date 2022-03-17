4e8b234303be73f7ab2747c8fb05b166c6e89c42
luis4PF <99516125+luis4PF@users.noreply.github.com>
2022-03-16 21:30:01 -0300

    Update resultado.php

--- listaExercicios-2/exercicio1/resultado.php
+++ listaExercicios-2/exercicio1/resultado.php
@@ -8,29 +8,29 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>EXERCICIO 2</title>
+    <title>RESULTADO 1</title>
   </head>
   <body class="container">
-    <h1>EXERCÍCIO 2</h1>
-    <form method="post">
-        <div class="row">
-            <div class="col">
-                <label for="valor1" class="label-control">
-                    INFORME O VALOR:
-                </label>
-                <input type="number" step="any" name="valor1[]" placeholder="R$" 
-                    id="valor1" value="<?=$valor1?>" class="form-control" />
-            </div>
-        </div>
-    </form>
+    <h1>RESULTADO 1</h1>
+
     <?php
-    $valor1 = [];
-    $valor1 = array();
-    for ($valor1 = 0; $valor1 < 5; $valor1++){
-        foreach($_POST['valor1'] as $val){
-            echo "$val\n";
+    for ($i = 1; $i <= 10; $i++) {
+        $vetor[$i] = $_POST["valor$i"];
+    }
+
+    $posicao = 0;
+    $maior = 0; //qualquer valor maior que 0 sera trocado
+
+    //var_dump($vetor);
+    foreach($vetor as $chave => $valor){
+        echo "POSIÇÃO DO VETOR: $chave, O SEU VALOR É: $valor\n";
+        echo "<br>";
+        if ($valor > $maior){
+          $maior = $valor;
+          $posicao = $chave;
         }
     }
+    echo "o maior valor é: $maior e esta na posição: $posicao";
     ?>
     <!-- Optional JavaScript; choose one of the two! -->
 
