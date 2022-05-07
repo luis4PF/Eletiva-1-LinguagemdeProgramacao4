7a68cf1364e89025445093bfc227e0c1fabb5e55
luis4PF <99516125+luis4PF@users.noreply.github.com>
2022-05-06 22:09:13 -0300

    commit

--- LUIS4PF/src/Controller/ExercicioController.php
+++ LUIS4PF/src/Controller/ExercicioController.php
@@ -8,7 +8,8 @@ class ExercicioController
         require_once("../src/View/exemplo.php");
     }
 
-    public static function exibirResultado(){
+    public static function exibirResultado()
+    {
         $valor1 = $_POST["valor1"];
         $valor2 = $_POST["valor2"];
         $soma = $valor1 + $valor2;
